2769390936bba3c7b84b2fa91ee1127841e747e1
Administrator <1757181650@qq.com>
2017-09-11 17:20:25 +0800

    第一次修改

---
+++ app/src/main/java/com/example/administrator/git/Bean.java
@@ -0,0 +1,9 @@
+package com.example.administrator.git;
+
+/**
+ * Created by Administrator on 2017\9\11 0011.
+ */
+public class Bean {
+
+
+}
--- app/src/main/java/com/example/administrator/git/MainActivity.java
+++ app/src/main/java/com/example/administrator/git/MainActivity.java
@@ -3,11 +3,23 @@ package com.example.administrator.git;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+
+    }
+
+    private void initView() {
+        ArrayList<String> ar = new ArrayList<String>();
+        for (int i = 0; i < 10; i++) {
+            ar.add("石星星");
+
+        }
     }
 }
